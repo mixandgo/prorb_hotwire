0fc2814386a8ee6344d3ffefd2447702897a0270
Cezar Halmagean <cezar@mixandgo.com>
2022-07-13 16:42:54 +0300

    Added turbo frame examples

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,4 +2,10 @@ class SiteController < ApplicationController
   def first; end
 
   def second; end
+
+  def third
+    redirect_to second_page_path
+  end
+
+  def fourth; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "/first", to: "site#first", as: :first_page
   get "/second", to: "site#second", as: :second_page
+  post "/third", to: "site#third", as: :third_page
+  get "/fourth", to: "site#fourth", as: :fourth_page
   root "site#first"
 end
