a4f76dadebafdfdbe9d03a2d2250403e4c771bc7
Cezar Halmagean <cezar@mixandgo.com>
2022-07-14 16:44:09 +0300

    Added turbo streams

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,11 +1,21 @@
 class SiteController < ApplicationController
-  def first; end
+  def first
+    @count = 3
+  end
 
-  def second; end
+  def second
+  end
 
   def third
-    redirect_to second_page_path
+    @name, @email, @age = person_params.values_at(:name, :email, :age)
+    @count = params[:count].to_i + 1
   end
 
   def fourth; end
+
+  private
+
+    def person_params
+      params.require(:person).permit(:name, :email, :age)
+    end
 end
